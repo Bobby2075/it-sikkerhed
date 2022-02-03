ebb765a7b5a09e49182f13951ad2f19a2f0aa674
Marcus kaa <mkaa2002@gmail.com>
2022-02-03 10:23:01 +0100

    Yes

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <nav className="header">
+            <h1>
+                IT-Sikkerhed
+            </h1>
+        </nav>
+    );
+}
+ 
+export default Header
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+
+        <div className="content">
+            <div className="row">
+                <h2 className="title col">Fysisk Sikkerhed</h2>
+                <p className="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquam exercitationem rem sed eveniet nihil magni rerum incidunt eligendi amet.</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home

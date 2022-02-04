f136f9e8463d77639554af48072a2a344ec5ff1b
Marcus Kaa <mkaa2002@gmail.com>
2022-02-04 09:58:29 +0100

    Updated shit
    brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+
 const Home = () => {
     return (
         <div className="wrapper">
@@ -27,7 +28,10 @@ const Home = () => {
 
                     <p className="txt">Det er også vigtigt at du jævnligt sikre dig at dine adgangskoder er opdateret. Din adgangskode burde være 16 tegn lang og indholde store, små, numre, og symboler for at opnå mest effektiv sikkerhed. Det forslårs også at du opdatere din adgangskode hver 3 måned. Du må IKKE skrive din kode ned i filer på din pc da det kan være en sikkerhedsrisiko, skriv dem ned på et styk papir eller download en kode manager. Det er også vigtigt ikke at bruge den samme kode flere steder.</p>
 
-                    <p className="txt"><strong>Antivirus</strong></p>
+                    <p className="txt"><strong>3. Antivirus</strong></p>
+
+                    <p className="txt">Antivirus program er meget vigtigt for at sikre dig mod trusler på internettet. Udover antivirus er sund fornuft også vigtig. Ikke hent programmer fra ukendte sider og lad være med at trykke på mistænkelige links</p>
+
                 </article>
             </div>
 
